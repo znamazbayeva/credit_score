9a5b7037ca3e79c339010c87cf9665aa6e4a2ac7
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2024-06-07 18:24:26 +0200

    fix

--- credit_score/urls.py
+++ credit_score/urls.py
@@ -20,7 +20,7 @@ from api.swagger import schema_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
